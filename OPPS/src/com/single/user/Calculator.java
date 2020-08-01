2b4c2599f00efaa2f260c5541499ab06ec3ccedf
SHREE <sanket9darekar9@gmail.com>
2020-08-01 11:04:27 +0530

    second commit

---
+++ OPPS/src/com/single/user/Calculator.java
@@ -0,0 +1,13 @@
+package com.single.user;
+
+public class Calculator extends Data
+{
+	void add()
+	{
+		System.out.println("ADD RESULT "+(a+b));
+	}
+	void sub()
+	{
+		System.out.println("SUB RESULT"+(a-b));
+	}
+}
